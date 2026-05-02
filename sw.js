45b64fecd78da842f1b38ae65728e28175f43074
Roywars <rgarcias234@gmail.com>
2026-05-02 23:13:07 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,6 @@
-const CACHE_NAME = "gymtracker-cache-v1";
-
-const urlsToCache = [
-  "/",
-  "/index.html"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open('v1').then(c=>c.addAll(['index.html'])));
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
